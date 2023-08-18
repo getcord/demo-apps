cd39583a5cfb504096be38e18746c946b3232453
Albert <33761650+AlbertQM@users.noreply.github.com>
2023-08-18 10:17:27 +0100

    Demo app: Add new "Document" app (1/n)
    This is the first step towards having a google-docs like demo app.
    I'll do the implementation in steps, to make it easier to review.
    
    In this first PR:
    - Copy pasted `static-content`
    - Renamed some bits to `document`
    - Created the document UI from the figma.
    
    In the next PRs, I'll be adding Cord components.
    
    Test Plan:
    Test app locally.
    
    <img width="1037" alt="image" src="https://github.com/getcord/monorepo/assets/33761650/47fed38a-c2c0-4911-b76a-2efa86badc7a">
    
    
    
    Reviewers: elenaperezrioja, flooey
    
    Reviewed By: elenaperezrioja
    
    Pull Request: https://github.com/getcord/monorepo/pull/5921
    
    monorepo-commit: 0747146e4e5517bcc1ca767b7f012cbbcbfb3ded

---
+++ document/src/App.tsx
@@ -0,0 +1,16 @@
+import { CordProvider } from '@cord-sdk/react';
+
+import { InformationHeader } from '../../_common/InformationHeader';
+import { useCordSampleToken_DEMO_ONLY_NOT_FOR_PRODUCTION } from './utils';
+import { Document } from './components/Document';
+
+export default function App() {
+  const authToken = useCordSampleToken_DEMO_ONLY_NOT_FOR_PRODUCTION();
+  return (
+    // All the Cord React components must be children of a single CordProvider component.
+    <CordProvider clientAuthToken={authToken}>
+      <InformationHeader api={[]} components={[]} />
+      {authToken && <Document />}
+    </CordProvider>
+  );
+}
---
+++ document/src/components/Document.tsx
@@ -0,0 +1,26 @@
+export const LOCATION = { page: 'document' };
+
+export function Document() {
+  return (
+    <div id="sheet">
+      <h1 id="title">What can you build with Cord?</h1>
+      <ul>
+        <li id="item-1">
+          Team chat experiences in your product -- letting your users talk to
+          each other (this is absurdly easy to build with Cord)
+        </li>
+        <li id="item-2">AI chatbot powered by ChatGPT and other LLMs</li>
+        <li id="item-3">Tasks integrations (for things like Jira or Asana)</li>
+        <li id="item-4">Integrations with your existing notifications</li>
+        <li id="item-5">
+          Customer support UIs baked right into your own products
+        </li>
+        <p id="body">
+          And so much more. Cord is about to launch its general availability,
+          open signup in the next few weeks. We&apos;re super excited to see
+          what collaborative experiences people create. 🌟🌟
+        </p>
+      </ul>
+    </div>
+  );
+}
