12a54a1beb89a074e25bbc1bb31c83765e56849a
Albert <33761650+AlbertQM@users.noreply.github.com>
2023-09-20 16:51:10 +0700

    Document demo: tweak presence position
    Rather than showing it at the top, show it in the middle.
    
    
    Reviewers: Netceer, flooey, mhtien
    
    Reviewed By: mhtien
    
    Pull Request: https://github.com/getcord/monorepo/pull/6190
    
    monorepo-commit: 23272aa8e4402f813a09919848e0fc72ccf99495

--- document/src/components/FloatingPresence.tsx
+++ document/src/components/FloatingPresence.tsx
@@ -16,20 +16,26 @@ export function FloatingPresence({
         const { locations } = u.ephemeral;
         // We made it so user can only be at one location at a time.
         const elementId = (locations?.[0]?.elementId ?? '') as string;
+        const elementRect = document
+          .getElementById(elementId)
+          ?.getBoundingClientRect();
+        if (!elementRect) {
+          return null;
+        }
+        const { top, left, height } = elementRect;
         return (
           <Avatar
             key={u.id}
             userId={u.id}
             style={{
               position: 'absolute',
-              top: document.getElementById(elementId)?.getBoundingClientRect()
-                .top,
-              left: `${
-                (document.getElementById(elementId)?.getBoundingClientRect()
-                  .left ?? 0) -
+              top: top + window.scrollY + height / 2, // Move it to the middle of the line.
+              transform: 'translateY(-50%)', // Center it visually.
+              left:
+                left -
+                window.scrollX -
                 AVATARS_GAP * 2 - // Move it to the left of the text
-                idx * AVATARS_GAP // Move each avatar a bit more to the left
-              }px`,
+                idx * AVATARS_GAP, // Move each avatar a bit more to the left
               zIndex: 1,
               transition: 'top  0.25s ease 0.1s',
               visibility:
