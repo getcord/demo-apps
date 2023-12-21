df438c2279f2665ffee778de44803ebc367149a3
Maylynn <maylynn@cord.com>
2023-12-21 11:22:17 +0000

    demo-apps: remove old canvas
    Removes old canvas code and references to it! 🥳
    
    To do:
    Renaming `canvas-new` to `canvas` and updating the urls will be in a follow up PR
    
    
    Test Plan:
    Everything compiles nicely
    Navigating to `.../canvas` goes to our 404 page
    
    Reviewers: elenaperezrioja, dmmiller, km-nur, nickfil22
    
    Reviewed By: dmmiller
    
    Pull Request: https://github.com/getcord/monorepo/pull/7700
    
    monorepo-commit: 0042d1f6c4becca7f2cebf1bd8aa73dfbd012223

--- canvas/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import * as ReactDOM from 'react-dom/client';
-import App from './App';
-import './css/index.css';
-import './css/cord-variables.css';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />,
-);
